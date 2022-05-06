f8e8e1305fa78df98d9ed4daf2c8b00b9bea096c
mcreteur <creteur.maxime@gmail.com>
2022-05-06 17:26:44 +0200

    Add connect wallet button using ethers

--- components/Landing.tsx
+++ components/Landing.tsx
@@ -1,8 +1,10 @@
 import { Box, Heading } from '@chakra-ui/react';
+import { Navbar } from './Navbar';
 
 export default function Landing() {
   return (
     <Box>
+      <Navbar />
       <Heading>Landing page</Heading>
     </Box>
   );
---
+++ components/Navbar.tsx
@@ -0,0 +1,6 @@
+import { FunctionComponent } from 'react';
+import { ConnectWalletButton } from './ConnectWalletButton';
+
+export const Navbar: FunctionComponent = () => {
+  return <ConnectWalletButton></ConnectWalletButton>;
+};
